a2a7f1640bb406a3b3147d8a43376c1b4b603bc0
Diego Prystupczuk <d.r.prystupczuk@gmail.com>
2021-02-08 14:38:04 -0300

    backend .net core api

---
+++ BackEnd/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }
+}
---
+++ BackEnd/Models/TarjetaCredito.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class TarjetaCredito
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varChar(100)")]
+        public String Titular { get; set; }
+
+        [Required]
+        [Column(TypeName = "varChar(16)")]
+        public String NumeroTarjeta { get; set; }
+
+        [Required]
+        [Column(TypeName = "varChar(5)")]
+        public String FechaExpiracion { get; set; }
+
+        [Required]
+        [Column(TypeName = "varChar(3)")]
+        public String CVV { get; set; }
+    }
+}
